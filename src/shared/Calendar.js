3f12aba7bc44635af96d5ae7974d495e40e0c396
JP Dillingham <jpdillingham@gmail.com>
2017-07-30 16:07:14 -0500

    Merged files from qccoders-siteV2 repo

---
+++ src/shared/Calendar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import BigCalendar from 'react-big-calendar'
+
+//styles 
+import '../styles/Calendar.css'
+
+BigCalendar.momentLocalizer(moment);
+
+const Calendar = props => (
+  <div className='Calendar'>
+    <BigCalendar
+      className='BigCalendar'
+      events={props.eventsList}
+      startAccessor='startDate'
+      endAccessor='endDate'
+    />
+  </div>
+);
+
+Calendar.propTypes = {
+    eventsList:PropTypes.array.isRequired
+}
+
+export default Calendar
