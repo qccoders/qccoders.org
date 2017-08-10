1a2e22439de212aeb3526afb01a8d7d628791051
Michael Liendo <model3volution@gmail.com>
2017-08-09 22:48:53 -0500

    refactor: add a way for text paragraphs to be shared across files

--- src/components/GroupDetails.js
+++ src/components/GroupDetails.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import FormattedParagraph from '../shared/FormattedParagraph'
 import { SocialIcon } from 'react-social-icons';
+import {groupDetailsIntro} from '../data/groupDetailsText'
+
 
 const GroupDetails = props => (
   <div className="GroupDetails">
-  
+    <FormattedParagraph text={groupDetailsIntro} />
   Want more information? Check us out online at any of the following:
     <SocialIcon url="https://www.meetup.com/QCCoders" />
     <SocialIcon url="https://www.facebook.com/qccoders" />
