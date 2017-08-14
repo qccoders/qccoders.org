7261ffa8e638747c7dea44dec0eba12e87b5102a
shafer949 <shafer949@gmail.com>
2017-08-13 21:29:25 -0500

    Social media icons updates

--- src/components/GroupDetails.js
+++ src/components/GroupDetails.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import FormattedParagraph from '../shared/FormattedParagraph'
-import { SocialIcon } from 'react-social-icons';
 import {groupDetailsIntro} from '../data/groupDetailsText'
-
+import SocialMediaIcons from '../shared/SocialMediaIcons'
+import socialMediaUrls from '../data/socialMediaIcons'
+import socialMediaNetwork from '../data/socialMediaIcons'
+import {groupContactInfo} from '../data/groupContactText'
+import '../styles/SocialMediaIcons.css'
 
 const GroupDetails = props => (
   <div className="GroupDetails">
     <FormattedParagraph text={groupDetailsIntro} />
-  Want more information? Check us out online at any of the following:
-    <SocialIcon url="https://www.meetup.com/QCCoders" />
-    <SocialIcon url="https://www.facebook.com/qccoders" />
-    <SocialIcon url=" https://github.com/qccoders" network="github"/>
-    <SocialIcon url="https://www.youtube.com/channel/UCj0ulaio6fCqFSV5_4P5zNg" />
-  Have questions? Reach out to us at info@qccoders.org.
+    <p>Want more information? Check us out online at any of the following:</p>
+    <SocialMediaIcons urls={socialMediaUrls} network={socialMediaNetwork}/>
+    <FormattedParagraph text={groupContactInfo} />
   </div>
 )
 
---
+++ src/data/socialMediaIcons.js
@@ -0,0 +1,7 @@
+export default [
+     'https://www.meetup.com/QCCoders',
+     'https://www.facebook.com/qccoders',
+     'https://github.com/qccoders',
+     'https://www.youtube.com/channel/UCj0ulaio6fCqFSV5_4P5zNg'
+
+]
