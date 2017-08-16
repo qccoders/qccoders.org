01a448271c1eec41cd820e230cbcb96734bb0509
Michael Liendo <model3volution@gmail.com>
2017-08-16 00:44:08 -0500

    feat: provided valid email, users will recieve an invite

--- src/shared/SlackEmailForm.js
+++ src/shared/SlackEmailForm.js
@@ -31,7 +31,8 @@ class SlackEmailForm extends Component {
         const inviteApi = `https://slack.com/api/users.admin.invite?resend=true&token=${slackToken}&email=${email}&channels=C2C6PRY0Y`
         axios.get(inviteApi)
             .then(res => {
-                console.log(res.data)
+                const {ok} = res.data
+                ok ? console.log('success') : console.log(res.data.error)
             })
             .catch(e => {
                 console.log('uh, something went wrong on our end')
