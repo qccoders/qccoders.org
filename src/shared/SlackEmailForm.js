acd2c56cf0159cbb955db1a9916980adcbab5582
Michael Liendo <model3volution@gmail.com>
2017-08-16 00:39:18 -0500

    feat: input field correctly determines if valid email

--- src/shared/SlackEmailForm.js
+++ src/shared/SlackEmailForm.js
@@ -1,6 +1,8 @@
+import axios from 'axios'
 import React, { Component } from 'react'
 import {slackToken} from '../credentials'
-import {isEmail} from 'validator/lib/isEmail'
+import isEmail from 'validator/lib/isEmail'
+
 
 class SlackEmailForm extends Component {
     state = {
@@ -8,19 +10,42 @@ class SlackEmailForm extends Component {
     }
 
     handleFormSubmit = e => {
+
+        const {inputText} = this.state
+
         e.preventDefault()
-        
+        console.log(isEmail(inputText))
+        isEmail(inputText) ? this.sendSlackInvite(inputText) : this.displayInputError()
 
     }
 
     handleInputChange = e => {
+
         this.setState({
             inputText: e.target.value
         })
         
     }
 
+    sendSlackInvite = (email) => {
+        const inviteApi = `https://slack.com/api/users.admin.invite?resend=true&token=${slackToken}&email=${email}&channels=C2C6PRY0Y`
+        axios.get(inviteApi)
+            .then(res => {
+                console.log(res.data)
+            })
+            .catch(e => {
+                console.log('uh, something went wrong on our end')
+            })
+    }
+
+    displayInputError = () => {
+
+        console.log('error')
+
+    }
+
     render() {
+
         return (
             <form onSubmit={this.handleFormSubmit}>
                 <input 
