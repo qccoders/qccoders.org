4390ac68d3897bbdad662b84debb6fdb9afca4bd
Michael Liendo <model3volution@gmail.com>
2017-08-16 00:16:07 -0500

    feat: added input component for slack email invite

--- src/shared/SlackEmailForm.js
+++ src/shared/SlackEmailForm.js
@@ -1,3 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {slackToken} from '../credentials'
+import {isEmail} from 'validator/lib/isEmail'
 
+class SlackEmailForm extends Component {
+    state = {
+        inputText: ""
+    }
+
+    handleFormSubmit = e => {
+        e.preventDefault()
+        
+
+    }
+
+    handleInputChange = e => {
+        this.setState({
+            inputText: e.target.value
+        })
+        
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleFormSubmit}>
+                <input 
+                    className='SlackEmailForm'
+                    type='email'
+                    placeholder='your-email@email.com'
+                    value={this.state.inputText}
+                    onChange={this.handleInputChange}
+                />
+            </form>
+        )
+    }
+}
+
+export default SlackEmailForm
