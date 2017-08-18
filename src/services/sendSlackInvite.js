0a42bed23e1934f507b6aa6e83b0da87e4008776
Michael Liendo <model3volution@gmail.com>
2017-08-17 22:31:57 -0500

    refactor: convert service to return a promise

--- src/services/sendSlackInvite.js
+++ src/services/sendSlackInvite.js
@@ -1,15 +1,14 @@
 import axios from 'axios'
 
-export const sendSlackInvite = (email, token) => {
+export const sendSlackInvite = (email = '', token = '') => {
 
     const inviteApi = `https://slack.com/api/users.admin.invite?resend=true&token=${token}&email=${email}&channels=C2C6PRY0Y`
     
-    axios.get(inviteApi)
+    return axios.get(inviteApi)
         .then(res => {
 
-            const {ok} = res.data
-
-            ok ? console.log('success') : console.log(res.data.error)
+            return res.data.ok
+        
         })
         .catch(e => {
 
