dc00e23d07d66d97842cc2fb9d765a2449f9a268
Michael Liendo <model3volution@gmail.com>
2017-10-09 12:42:33 -0500

    feat: add react-headroom for nav

--- src/shared/NavBar/index.js
+++ src/shared/NavBar/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import SlackForm from './SlackEmailForm'
 import logo from '../../img/logos/qccoder_logo.png'
-
+import Headroom from 'react-headroom'
+ 
 import './index.css'
 
 const NavBar = props => (
+  <Headroom>
     <nav className='NavBar navbar navbar-dark bg-dark'>
         <NavLink exact to='/' className='navbar-brand' activeClassName='nav-is-active'>
           <img alt='qccoders' src={logo} height={90} width={130}/>
@@ -15,6 +17,7 @@ const NavBar = props => (
         <NavLink to='/job-board' className='nav-item' activeClassName='nav-is-active'>Job Board</NavLink>
         <SlackForm />
     </nav>
+  </Headroom>
 )
 
 export default NavBar 
