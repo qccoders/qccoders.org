a83b81abcbd2262e35e63492d76403610ec89318
Michael Liendo <model3volution@gmail.com>
2017-10-09 14:06:05 -0500

    refactor: move icons into header component

--- src/components/Home/GroupDetails/index.js
+++ src/components/Home/GroupDetails/index.js
@@ -1,9 +1,6 @@
 import React from 'react'
 import FormattedParagraph from '../../../services/FormattedParagraph'
-import {groupDetailsIntro} from '../../../data/groupDetailsText'
-import SocialMediaIcons from '../../../shared/SocialMediaIcons'
-import socialMediaUrls from '../../../shared/SocialMediaIcons/urls' 
-import {groupContactInfo} from '../../../data/groupContactText'
+import {groupDetailsIntro} from '../../../data/groupDetailsText' 
 
 //styles
 import './index.css'
@@ -12,9 +9,6 @@ const GroupDetails = props => (
   <div className="GroupDetails">
     <h1>Who are we? Glad you asked!</h1>
     <FormattedParagraph text={groupDetailsIntro} />
-    <p>Want more information? Check us out online at any of the following:</p>
-    <SocialMediaIcons urls={socialMediaUrls}/>
-    <FormattedParagraph text={groupContactInfo} />
   </div>
 )
 
--- src/shared/Footer/index.js
+++ src/shared/Footer/index.js
@@ -1,4 +1,15 @@
 import React from 'react'
+import SocialMediaIcons from '../SocialMediaIcons'
+import socialMediaUrls from '../SocialMediaIcons/urls'
+import FormattedParagraph from '../../services/FormattedParagraph'
+import {groupContactInfo} from '../../data/groupContactText'
+
+//styles
 import './index.css'
 
-export default props => <div>This will show up as the footer on every page</div>
+export default props => (
+  <div className='container-flud Footer'>
+    <SocialMediaIcons urls={socialMediaUrls}/>
+    <FormattedParagraph text={groupContactInfo} />
+  </div>
+)
