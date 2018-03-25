788e654ccd032221832bfc74f1d21bb41701da62
ML73394 <mliendo@aquent.com>
2018-03-24 23:04:19 -0500

    fix: add bootstrap default styling

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './App/routes'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 ReactDOM.render(<Routes />, document.getElementById('root'));
