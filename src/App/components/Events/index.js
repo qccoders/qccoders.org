3920e6c0fd2117e1eb95f4cac8cccfcf8841f422
Seth Watkins <dev.seth.wat@gmail.com>
2018-04-09 16:07:18 -0500

    feat: Fetch event data from service, update component state

--- src/App/components/Events/index.js
+++ src/App/components/Events/index.js
@@ -6,10 +6,21 @@ import moment from 'moment'
 import './index.css'
 
 const currentMonth = moment().format('MMMM')
+const eventURL = 'http://localhost:3001/events'
+const parseJson = (jsonResponse) => jsonResponse.json();
+const logData = (json) => console.log(json)
+const formatData = (json) => 
+    json.map((event, index) => ({id: event.index, title: event.title, allDay: false, start: event.startDate, end: event.endDate}));
 
+    
 class Events extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {eventList: []};
+    }
+
+    componentDidMount() {
+        fetch(eventURL).then(parseJson).then(formatData).then((eventData) => this.setState({eventList: eventData}));
     }
 
     render() {
@@ -19,7 +30,7 @@ class Events extends React.Component {
                 <div className='container'>
                     <div className='col'>
                         <h3 className='text-center'>{currentMonth}'s Events</h3>
-                        <Calendar toolbar={false}/>
+                        <Calendar toolbar={false} events={this.state.eventList} />
                     </div>
                 </div>
             </div>
