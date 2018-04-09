321f0dc22d55081a0b91237aebe08b38e2bd4b20
Seth Watkins <dev.seth.wat@gmail.com>
2018-04-09 16:03:00 -0500

    refactor: Switched Events componet from function to class

--- src/App/components/Events/index.js
+++ src/App/components/Events/index.js
@@ -6,15 +6,25 @@ import moment from 'moment'
 import './index.css'
 
 const currentMonth = moment().format('MMMM')
-export default (props) => (
-    <div className='Events'>
-        <div className='row event-calendar'>
-            <div className='container'>
-                <div className='col'>
-                    <h3 className='text-center'>{currentMonth}'s Events</h3>
-                    <Calendar toolbar={false}/>
+
+class Events extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className='Events'>
+            <div className='row event-calendar'>
+                <div className='container'>
+                    <div className='col'>
+                        <h3 className='text-center'>{currentMonth}'s Events</h3>
+                        <Calendar toolbar={false}/>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-)
+        )
+    }
+}
+export default (props) => new Events(props);
