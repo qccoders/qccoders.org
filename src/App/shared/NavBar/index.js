9bd46b35f8b386352e01f7136dbf92404e3dbb5e
Seth Watkins <dev.seth.wat@gmail.com>
2018-04-17 10:20:37 -0500

    feat: Change nav background to white, update text

--- src/App/shared/NavBar/index.js
+++ src/App/shared/NavBar/index.js
@@ -6,12 +6,16 @@ import Headroom from 'react-headroom'
 
 import './index.css'
 
+const eventText = "{ Events }"
+const missionText = "{ Mission }"
+const jobText = "{ Job Board }"
+
 const NavBar = props => (
     <Headroom>
-      <nav className='NavBar navbar navbar-dark bg-dark'>
-        <NavLink to='/events' className='nav-item' activeClassName='nav-is-active'>Events</NavLink>
-        <NavLink to='/mission' className='nav-item' activeClassName='nav-is-active'>Mission</NavLink>
-        <NavLink to='/job-board' className='nav-item' activeClassName='nav-is-active'>Job Board</NavLink>
+      <nav className='NavBar navbar navbar-dark nav'>
+        <NavLink to='/events' className='nav-item' activeClassName='nav-is-active'>{eventText}</NavLink>
+        <NavLink to='/mission' className='nav-item' activeClassName='nav-is-active'>{missionText}</NavLink>
+        <NavLink to='/job-board' className='nav-item' activeClassName='nav-is-active'>{jobText}</NavLink>
       </nav>
     </Headroom>
 )
