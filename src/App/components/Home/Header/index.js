1cd7e0e4f79929de0099c4aa385871247b750fdb
JP Dillingham <jpdillingham@gmail.com>
2018-09-30 14:49:33 -0500

    resize images and add to carousel

--- src/App/components/Home/Header/index.js
+++ src/App/components/Home/Header/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import {Carousel} from 'react-responsive-carousel'
 import logo1 from '../../../img/carousel/anniversary_cake.jpg'
-import logo2 from '../../../img/carousel/casual.jpg'
+import logo2 from '../../../img/carousel/casual.jpeg'
 import logo3 from '../../../img/carousel/presentation.jpg'
-import logo4 from '../../../img/logos/qccoder_logo.png'
+import logo4 from '../../../img/carousel/scrum.jpg'
 
 //styles
 import 'react-responsive-carousel/lib/styles/carousel.min.css'
@@ -15,9 +15,19 @@ export default (props) => (
       <img src={logo1} alt='anniversary cake'/>
       <p></p>
     </div>
-   
+
+    <div>
+      <img src={logo2} alt='casual hangout'/>
+      <p></p>
+    </div>
+
     <div>
       <img src={logo3} alt='meetup presentation'/>
+      <p></p>
+    </div>
+
+    <div>
+      <img src={logo4} alt='scrum presentation'/>
       <p></p>
     </div>
   </Carousel>
