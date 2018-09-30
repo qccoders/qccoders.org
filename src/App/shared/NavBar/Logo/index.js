238bc786fd2d0f1293b36dd6ae880733fc3b57cf
JP Dillingham <jpdillingham@gmail.com>
2018-09-30 15:01:04 -0500

    set background to white, change logo, remove Headroom

--- src/App/shared/NavBar/Logo/index.js
+++ src/App/shared/NavBar/Logo/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from '../../../img/logos/qccoder_logo.png'
+import logo from '../../../img/logos/logo.jpg'
 import './index.css'
 
 const Logo = () => {
--- src/App/shared/NavBar/index.js
+++ src/App/shared/NavBar/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import Headroom from 'react-headroom'
 import Logo from './Logo'
 
 import './index.css'
@@ -10,15 +9,12 @@ const missionText = "Mission"
 const jobText = "Job Board"
 
 const NavBar = props => (
-    <Headroom>
-
       <nav className='NavBar navbar navbar-dark nav'>
         <NavLink to='/' className='nav-item'><Logo/></NavLink>
         <NavLink to='/events' className='nav-item' activeClassName='nav-is-active'>{eventText}</NavLink>
         <NavLink to='/mission' className='nav-item' activeClassName='nav-is-active'>{missionText}</NavLink>
         <NavLink to='/job-board' className='nav-item' activeClassName='nav-is-active'>{jobText}</NavLink>
       </nav>
-    </Headroom>
 )
 
 export default NavBar 
