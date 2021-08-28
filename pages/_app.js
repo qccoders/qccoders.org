e51de4441374110bcf9cde4b329ca34763f08801
Seth Ryan Watkins <seth.watkins.000@gmail.com>
2021-08-28 13:29:23 -0500

    Add qccoders favicon

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css2?family=Amaranth:wght@700&family=Open+Sans:wght@400;600;700&family=Yeseva+One&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
